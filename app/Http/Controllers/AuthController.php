82f13aed47a0e3568a38e57fc9ccc823fdd89c9d
Anastasya <work.anastasyaa@gmail.com>
2025-05-20 00:17:23 +0800

    api register user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    // buat test api aja ini
+    public function getUser() {
+
+        $user = User::all();
+        return response()->json($user);
+
+    }
+
+    // daftar akun (buat akun)
+    public function register(Request $request) 
+    {
+        $validated = $request->validate([
+            'nama' => 'required|string|max:255',
+            'no_telp' => 'required|string|max:20',
+            'email' => 'required|email|unique:user,email|max:255',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+            'nama' => $validated['nama'],
+            'no_telp' => $validated['no_telp'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+            'role' => $validated['role'] ?? 'wisatawan',
+        ]);
+
+        return response()->json([
+            'data' => $user
+        ], 201);
+        
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/user',UserController::class);
+// Route::apiResource('/user',UserController::class);
+
+Route::get('user', [AuthController::class, 'getUser']); // buat test aja ini
+Route::post('user', [AuthController::class, 'register']);
