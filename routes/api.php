7d05a4eaa21ceea9b2cf3ef6efef258ef63e79e0
Anastasya <work.anastasyaa@gmail.com>
2025-05-23 15:46:58 +0800

    t

--- routes/api.php
+++ routes/api.php
@@ -13,46 +13,14 @@ Route::get('/user', function (Request $request) {
 
 Route::get('user', [AuthController::class, 'getUser']); // buat test aja ini
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// Route publik
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-// Route dengan token valid
-=======
-// Autentikasi
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-// Hanya bisa diakses dengan token valid
->>>>>>> Stashed changes
-=======
-// Autentikasi
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-// Hanya bisa diakses dengan token valid
->>>>>>> Stashed changes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::patch('/profile', [AuthController::class, 'updateProfile']);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 });
-=======
-=======
->>>>>>> Stashed changes
-    
-    // Ganti password
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-<<<<<<< Updated upstream
-});
->>>>>>> Stashed changes
-=======
-});
->>>>>>> Stashed changes
+
